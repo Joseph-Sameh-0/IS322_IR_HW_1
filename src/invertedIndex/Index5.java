ab052180ebfacbd2787e45ce02ce0f980c0449cc
Amany Mohamed <147101273+Eng-AmanyMohamed@users.noreply.github.com>
2025-03-15 12:50:58 +0200

    Update Index5.java

--- src/invertedIndex/Index5.java
+++ src/invertedIndex/Index5.java
@@ -137,7 +137,9 @@ public class Index5 {
     }
 
     //----------------------------------------------------------------------------
+    //stopWord(String word) is a Boolean function that checks whether a given word is a stop word or not
     boolean stopWord(String word) {
+    // Check if the word is in the list of common stop words (the,to,be,for,from,in,a,into,by,or,and,that)
         if (word.equals("the") || word.equals("to") || word.equals("be") || word.equals("for") || word.equals("from") || word.equals("in")
                 || word.equals("a") || word.equals("into") || word.equals("by") || word.equals("or") || word.equals("and") || word.equals("that")) {
             return true;
